69fe8ec56550739e6b567b3dee63fb2e103f1bab
Luis <l-serrato@live.com>
2023-07-17 17:52:33 -0300

    req7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for i in range(0, len(instance)):
+        file = instance.search(i)
+        insts = {
+            "palavra": word,
+            "arquivo": file["nome_do_arquivo"],
+            "ocorrencias": list(),
+        }
+
+        for line, text in enumerate(file["linhas_do_arquivo"]):
+            if word.casefold() in text.casefold():
+                insts["ocorrencias"].append({"linha": line + 1})
+
+        if len(insts["ocorrencias"]) > 0:
+            result.append(insts)
+
+    return result
 
 
 def search_by_word(word, instance):
