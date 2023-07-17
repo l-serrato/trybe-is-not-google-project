b12242a429547898d56ef05443dd5dcf39d800dc
Luis <l-serrato@live.com>
2023-07-17 16:00:35 -0300

    req2 test

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import os
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return None
+
+    with open(path_file, "r") as file:
+        content = file.read()
+        return content.split("\n")
