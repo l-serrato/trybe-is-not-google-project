81cb1971cda32a1317400e760b111a54b4e558e7
Luis <l-serrato@live.com>
2023-07-17 16:36:26 -0300

    req3 test

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import os
+from ting_file_management.file_management import txt_importer
+
+
+def process(queue, file_path):
+    file_name = os.path.basename(file_path)
+
+    if queue.contains(file_name):
+        return
+
+    data = txt_importer(file_path)
+    if not data:
+        return
+
+    processed_data = {
+        'nome_do_arquivo': file_name,
+        'qtd_linhas': len(data),
+        'linhas_do_arquivo': data
+    }
+
+    queue.enqueue(file_name)
+
+    print(processed_data)
 
 
 def remove(instance):
